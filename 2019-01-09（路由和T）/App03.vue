679aaa0391408664dc9d8ad7c01f33ac852e7c2f
guoyh007 <754308574@qq.com>
2019-01-10 21:22:57 +0800

    2019/01/10

---
+++ 2019-01-09（路由和T）/App03.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <router-link to="a" tag="button">aaa</router-link>
+    <router-link to="b" tag="button">bbb</router-link>
+    <router-link to="c" tag="button">ccc</router-link>
+    {{$route.params.id}}
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  // watch: {
+  //   $route(){
+  //     console.log(this.$route)
+  //   }
+  // },
+//   {name: undefined, meta: {…}, path: "/c", hash: "", query: {…}, …}
+
+// fullPath: "/c"
+// hash: ""
+// matched: [{…}]
+// meta: {}
+// name: undefined
+// params: {}
+// path: "/c"
+// query: {}
+// __proto__: Object
+}
+</script>
